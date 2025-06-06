b949483d63edc0819c4c897bfbd4f018dfa2384f
pedrobicas <pedro.bicas14@gmail.com>
2025-06-06 09:29:43 -0300

    open api

---
+++ src/main/java/com/example/powerguard/config/OpenApiConfig.java
@@ -0,0 +1,33 @@
+package com.example.powerguard.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("PowerGuard API")
+                        .description("API para monitoramento de falhas de energia e impactos cibernéticos")
+                        .version("1.0")
+                        .contact(new Contact()
+                                .name("PowerGuard Team")
+                                .email("contato@powerguard.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://www.apache.org/licenses/LICENSE-2.0.html")))
+                .schemaRequirement("bearerAuth", new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT"));
+    }
+} 
